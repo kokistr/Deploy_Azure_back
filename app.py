c4e9d5c07f41ccc95f89bb80085f2d48996e3312
kokistr <125700374+kokistr@users.noreply.github.com>
2024-11-21 16:04:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 # CORS設定を更新
-CORS(app, resources={
-    r"/api/*": {
-        "origins": [
-            "http://localhost:3000",
-            "https://techo-gen-8-step3-testapp-node2-28.azurewebsites.net"
-        ]
-    }
-})
+CORS(app, resources={r"/api/*": {"origins": "https://techo-gen-8-step3-testapp-node2-28.azurewebsites.net"}})  # CORS設定を更新
 
 @app.route('/', methods=['GET'])
 def hello():
